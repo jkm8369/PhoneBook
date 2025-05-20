500b0c394c82c421c4eea5889386fe86a5d94031
jkm8369 <jkm8369@naver.com>
2025-05-20 10:29:31 +0900

    2025-05-20

---
+++ src/com/javaex/ex/PhoneApp.java
@@ -0,0 +1,31 @@
+package com.javaex.ex;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PhoneApp {
+
+	public static void main(String[] args) throws IOException{
+
+ 		List<Person> pList= new ArrayList<Person>();
+ 		
+ 		InputStream in = new FileInputStream("C:\\javaStudy\\PhoneDB_원본.txt");
+ 		InputStreamReader isr = new InputStreamReader(in);
+ 		BufferedReader br = new BufferedReader(isr);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
